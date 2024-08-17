36b847301316a75635f0f55b3d13028641a9c9eb
Roman Kotenko <r.d.kotenko@gmail.com>
2024-08-17 00:44:57 -0400

    Add script for listing USB /dev/ files

---
+++ scripts/ls-usb-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Prints a mapping of active /dev/ entries to their device names.
+# https://unix.stackexchange.com/a/144735
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && exit
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && exit
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
