15506ba48503de07c09cda68727b71c0cd821daf
Roman Kotenko <r.d.kotenko@gmail.com>
2024-11-15 21:59:38 -0500

    Fix black flickering by switching to beta nvidia driver

--- configuration.nix
+++ configuration.nix
@@ -54,15 +54,24 @@ in {
     driSupport = true;
     driSupport32Bit = true;
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # Version 555 introduced explicit sync which fixes black flickering in some GUIs. Once stable reaches 555, revert to
+    # `config.boot.kernelPackages.nvidiaPackages.stable`.
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58.02";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   # Sound config
   security.rtkit.enable = true;  # Optional but recommended.
