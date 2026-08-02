14c1113a60b0cae4498319dbd482e5c1c3e8ba22
Roman Kotenko <r.d.kotenko@gmail.com>
2026-08-02 16:07:36 -0400

    Add basic neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+
