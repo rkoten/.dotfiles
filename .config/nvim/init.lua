4d55469a10ec89af84b80aaff9f11dae2024f66c
Roman Kotenko <r.d.kotenko@gmail.com>
2026-08-05 18:41:19 -0400

    nvim: set up vim.pack, install which-key plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,28 @@
-vim.opt.autoindent = true
-vim.opt.cursorline = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
-vim.opt.smarttab = true
+vim.loader.enable()  -- enable Neovim's Lua module cache
 
+vim.g.mapleader = ' '
+
+vim.o.autoindent = true
+vim.o.cursorline = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.smarttab = true
+vim.o.timeout = true
+vim.o.timeoutlen = 500  -- ms
+
+vim.pack.add({
+    { src = 'https://github.com/folke/which-key.nvim', version = vim.version.range('3.x') },
+})
+
+local wk = require('which-key')
+wk.setup({ preset = 'classic' })
+vim.keymap.set(
+    'n',
+    '<leader>?',
+    function()
+	wk.show({ global = false })
+    end,
+    { desc = 'Buffer local keymaps (which-key)' }
+)
